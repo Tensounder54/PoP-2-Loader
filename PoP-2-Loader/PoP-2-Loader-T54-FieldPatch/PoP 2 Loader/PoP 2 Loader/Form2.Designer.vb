b661a524adff8a802b41b7f857d5b78c87565754
Tensounder54 <Tensounder54@users.noreply.github.com>
2017-03-29 23:49:00 +0100

    Add files via upload

---
+++ PoP-2-Loader/PoP-2-Loader-T54-FieldPatch/PoP 2 Loader/PoP 2 Loader/Form2.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label51 = New System.Windows.Forms.Label()
+        Me.Label52 = New System.Windows.Forms.Label()
+        Me.Label53 = New System.Windows.Forms.Label()
+        Me.Label54 = New System.Windows.Forms.Label()
+        Me.Label55 = New System.Windows.Forms.Label()
+        Me.Label56 = New System.Windows.Forms.Label()
+        Me.Label57 = New System.Windows.Forms.Label()
+        Me.Label58 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label51
+        '
+        Me.Label51.AutoSize = True
+        Me.Label51.Location = New System.Drawing.Point(195, 335)
+        Me.Label51.Name = "Label51"
+        Me.Label51.Size = New System.Drawing.Size(123, 13)
+        Me.Label51.TabIndex = 146
+        Me.Label51.Text = "Advance To Next Level:"
+        '
+        'Label52
+        '
+        Me.Label52.AutoSize = True
+        Me.Label52.Location = New System.Drawing.Point(195, 308)
+        Me.Label52.Name = "Label52"
+        Me.Label52.Size = New System.Drawing.Size(115, 13)
+        Me.Label52.TabIndex = 145
+        Me.Label52.Text = "Toggle Ambient Music:"
+        '
+        'Label53
+        '
+        Me.Label53.AutoSize = True
+        Me.Label53.Location = New System.Drawing.Point(195, 286)
+        Me.Label53.Name = "Label53"
+        Me.Label53.Size = New System.Drawing.Size(112, 13)
+        Me.Label53.TabIndex = 144
+        Me.Label53.Text = "Restore Saved Game:"
+        '
+        'Label54
+        '
+        Me.Label54.AutoSize = True
+        Me.Label54.Location = New System.Drawing.Point(195, 260)
+        Me.Label54.Name = "Label54"
+        Me.Label54.Size = New System.Drawing.Size(127, 13)
+        Me.Label54.TabIndex = 143
+        Me.Label54.Text = "Activate Keyboard Mode:"
+        '
+        'Label55
+        '
+        Me.Label55.AutoSize = True
+        Me.Label55.Location = New System.Drawing.Point(195, 187)
+        Me.Label55.Name = "Label55"
+        Me.Label55.Size = New System.Drawing.Size(88, 13)
+        Me.Label55.TabIndex = 142
+        Me.Label55.Text = "Save The Game:"
+        '
+        'Label56
+        '
+        Me.Label56.AutoSize = True
+        Me.Label56.Location = New System.Drawing.Point(195, 234)
+        Me.Label56.Name = "Label56"
+        Me.Label56.Size = New System.Drawing.Size(120, 13)
+        Me.Label56.TabIndex = 141
+        Me.Label56.Text = "Activate Joystick Mode:"
+        '
+        'Label57
+        '
+        Me.Label57.AutoSize = True
+        Me.Label57.Location = New System.Drawing.Point(195, 210)
+        Me.Label57.Name = "Label57"
+        Me.Label57.Size = New System.Drawing.Size(108, 13)
+        Me.Label57.TabIndex = 140
+        Me.Label57.Text = "Display Hall Of Fame:"
+        '
+        'Label58
+        '
+        Me.Label58.Location = New System.Drawing.Point(195, 145)
+        Me.Label58.Name = "Label58"
+        Me.Label58.Size = New System.Drawing.Size(127, 38)
+        Me.Label58.TabIndex = 139
+        Me.Label58.Text = "Restart Level Or Go To Last Rebirth Point:"
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(516, 492)
+        Me.Controls.Add(Me.Label51)
+        Me.Controls.Add(Me.Label52)
+        Me.Controls.Add(Me.Label53)
+        Me.Controls.Add(Me.Label54)
+        Me.Controls.Add(Me.Label55)
+        Me.Controls.Add(Me.Label56)
+        Me.Controls.Add(Me.Label57)
+        Me.Controls.Add(Me.Label58)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label51 As Label
+    Friend WithEvents Label52 As Label
+    Friend WithEvents Label53 As Label
+    Friend WithEvents Label54 As Label
+    Friend WithEvents Label55 As Label
+    Friend WithEvents Label56 As Label
+    Friend WithEvents Label57 As Label
+    Friend WithEvents Label58 As Label
+End Class
